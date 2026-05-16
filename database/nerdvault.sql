9ed36f26d6e0c038d62a2276fa2c5a8323f67e70
Tepig02 <paolocalabrese80@gmail.com>
2026-05-16 20:10:26 +0200

    Database Paolo

--- database/nerdvault.sql
+++ database/nerdvault.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mag 16, 2026 alle 19:55
+-- Creato il: Mag 16, 2026 alle 20:09
 -- Versione del server: 10.4.32-MariaDB
 -- Versione PHP: 8.2.12
 
@@ -57,13 +57,6 @@ CREATE TABLE `admin` (
   `data_creazione` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dump dei dati per la tabella `admin`
---
-
-INSERT INTO `admin` (`id_admin`, `email`, `password_hash`, `livello_sicurezza`, `data_creazione`) VALUES
-(2, 'admin@nerdvault.it', '$2y$10$0bhLHjGaBb20aiNx.eeUVeOJYnhz06l6HKbNAm9rG6HWutWE.38Aq', 2, '2026-05-15 13:52:15');
-
 -- --------------------------------------------------------
 
 --
@@ -85,17 +78,6 @@ CREATE TABLE `annuncio` (
   `data_scadenza` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dump dei dati per la tabella `annuncio`
---
-
-INSERT INTO `annuncio` (`id_annuncio`, `id_utente`, `id_business`, `id_categoria`, `titolo`, `descrizione`, `stato_conservazione`, `prezzo`, `modalita_consegna`, `stato`, `data_creazione`, `data_scadenza`) VALUES
-(4, 2, NULL, 8, 'a', 's', 'Nuovo', 21.00, 'Consegna', 'venduto', '2026-05-15 14:32:27', NULL),
-(5, 2, NULL, 8, 'a', 'asd', '', 21.00, 'Consegna', 'attivo', '2026-05-16 11:44:07', NULL),
-(6, 2, NULL, 25, 'a', 'dasd', '', 21.00, 'Consegna', 'attivo', '2026-05-16 16:12:21', NULL),
-(7, 2, NULL, 30, 'asdasdasda', 'basdasdasdasd', '', 99999999.99, 'Consegna', 'attivo', '2026-05-16 16:12:46', NULL),
-(8, 2, NULL, 3, 'fsdfsd', 'sdfsdfsd', '', 99999999.99, 'Consegna', 'attivo', '2026-05-16 16:13:36', NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -108,14 +90,6 @@ CREATE TABLE `carrello` (
   `data_creazione` timestamp NOT NULL DEFAULT current_timestamp(),
   `data_aggiornamento` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dump dei dati per la tabella `carrello`
---
-
-INSERT INTO `carrello` (`id_carrello`, `id_utente`, `data_creazione`, `data_aggiornamento`) VALUES
-(2, 2, '2026-05-15 13:22:46', '2026-05-15 13:22:46'),
-(3, 3, '2026-05-15 14:42:18', '2026-05-15 14:42:18');
 
 -- --------------------------------------------------------
 
@@ -220,13 +194,6 @@ CREATE TABLE `feedback` (
   `data_feedback` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dump dei dati per la tabella `feedback`
---
-
-INSERT INTO `feedback` (`id_feedback`, `id_autore`, `id_pagamento`, `valutazione`, `commento`, `data_feedback`) VALUES
-(1, 3, 1, 5, 'Ngulo che merda', '2026-05-15 14:51:48');
-
 -- --------------------------------------------------------
 
 --
@@ -239,16 +206,6 @@ CREATE TABLE `immagine` (
   `url` varchar(500) NOT NULL,
   `ordine` tinyint(4) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dump dei dati per la tabella `immagine`
---
-
-INSERT INTO `immagine` (`id_immagine`, `id_annuncio`, `url`, `ordine`) VALUES
-(4, 4, 'uploads/annunci/4/f7f467169f6299e0c92f624d38dafc65.png', 0),
-(5, 5, 'uploads/annunci/5/bf137682b9a44f7c0a291249be530bc6.jpg', 0),
-(6, 7, 'uploads/annunci/7/039ced1e82ea71ff314a2cb006781231.png', 0),
-(7, 8, 'uploads/annunci/8/3d22ab1fe5e4cdab95592e590662a96a.png', 0);
 
 -- --------------------------------------------------------
 
@@ -269,15 +226,6 @@ CREATE TABLE `indirizzi` (
   `paese` varchar(50) NOT NULL DEFAULT 'Italia',
   `predefinito` tinyint(1) NOT NULL DEFAULT 0
 ) ;
-
---
--- Dump dei dati per la tabella `indirizzi`
---
-
-INSERT INTO `indirizzi` (`id_indirizzo`, `id_utente`, `id_business`, `tipo`, `via`, `numero`, `cap`, `citta`, `provincia`, `paese`, `predefinito`) VALUES
-(4, 2, NULL, 'spedizione', 'Via Asmara', '39', '67100', 'L\'Aquila', 'AQ', 'Italia', 1),
-(6, 2, NULL, 'casa', 'Via Asmara', '39', '67100', 'L\'Aquila', 'AQ', 'Italia', 0),
-(7, 3, NULL, 'casa', 'Via Asmara', '39', '67100', 'L\'Aquila', 'AQ', 'Italia', 1);
 
 -- --------------------------------------------------------
 
@@ -312,13 +260,6 @@ CREATE TABLE `pagamento` (
   `data` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dump dei dati per la tabella `pagamento`
---
-
-INSERT INTO `pagamento` (`id_pagamento`, `id_annuncio`, `id_acquirente`, `importo_totale`, `stato`, `paypal_transaction_id`, `data`) VALUES
-(1, 4, 3, 21.00, 'Completato', 'asd', '2026-05-15 14:44:26');
-
 -- --------------------------------------------------------
 
 --
@@ -333,14 +274,6 @@ CREATE TABLE `password_reset` (
   `usato` tinyint(1) NOT NULL DEFAULT 0,
   `creato_il` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dump dei dati per la tabella `password_reset`
---
-
-INSERT INTO `password_reset` (`id_reset`, `id_utente`, `token`, `scadenza`, `usato`, `creato_il`) VALUES
-(1, 12, '863c8de06636662ae472d7f9e0fb75bbdfb61ee16b79f3dbbeeaeb67ad8a12f8', '2026-05-16 20:51:13', 1, '2026-05-16 19:51:13'),
-(2, 12, '544e895b6aac8b6f95ab6c5f6ec90e4deb1a440b06b514b0b858bfee4bf1ba43', '2026-05-16 20:52:26', 1, '2026-05-16 19:52:26');
 
 -- --------------------------------------------------------
 
@@ -395,15 +328,6 @@ CREATE TABLE `utente_registrato` (
   `stato_ban` tinyint(1) NOT NULL DEFAULT 0,
   `data_registrazione` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dump dei dati per la tabella `utente_registrato`
---
-
-INSERT INTO `utente_registrato` (`id_utente`, `email`, `email_verificata`, `token_verifica`, `token_scadenza`, `username`, `password_hash`, `nome`, `telefono`, `propic`, `stato_ban`, `data_registrazione`) VALUES
-(2, 'a@gmail.com', 1, NULL, NULL, 'a', '$2y$10$Rggdvl86uMH5dWy0acCQGuNHxBzH6NFHod0r2Dq1WXp4SElriKpR.', 'Paolo Calabrese', '2', 'uploads/propic/user_2_4b0ac4145b5df87a.jpg', 0, '2026-05-15 12:21:24'),
-(3, 'c@gmail.com', 1, NULL, NULL, 'c', '$2y$10$B9wSxWfGTfArHE.uUE.Sdeo026aoZRupewjQ5M.WPJLJArRg5t.6u', 'Paolo Calabrese', '2', NULL, 0, '2026-05-15 13:54:41'),
-(12, 'accpakun99@gmail.com', 1, NULL, NULL, 'KiritoKun', '$2y$10$XJ1axsWkO.wcb7gLO4hCuOlDzdvz/0Y5LEC19aTfdYppQBhGIc8jO', NULL, '3891925959', NULL, 0, '2026-05-16 17:44:01');
 
 --
 -- Indici per le tabelle scaricate
