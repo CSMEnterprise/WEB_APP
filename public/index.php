874296f4cef2a092a34a3a671b795ae8e5846f92
SickAdry <83632796+SickAdry@users.noreply.github.com>
2026-05-30 02:59:04 +0200

    pulizia require_once in index

--- public/index.php
+++ public/index.php
@@ -8,10 +8,6 @@ session_start();
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/config/db.php';
-require_once __DIR__ . '/../src/middleware/auth.php';
-require_once __DIR__ . '/../src/middleware/admin.php';
-require_once __DIR__ . '/../src/middleware/business.php';
-require_once __DIR__ . '/../src/middleware/guest.php';
 
 FDataBase::init($pdo);
 
